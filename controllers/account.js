e4bf2d4b533fe22211e15367161fdb2dd3af2d20
Denix7 <dennis.brian.pv@gmail.com>
2019-12-12 21:11:02 -0400

    Save and edit Profile and address

--- controllers/account.js
+++ controllers/account.js
@@ -12,11 +12,11 @@ module.exports =
     saveUser: function(req, res, next)
     {
         let user = new User();
-        // user.name = req.body.name;
+        user.name = req.body.name;
         user.email = req.body.email;
         user.password = req.body.password;
-        // user.picture = user.gravatar();
-        // user.isSeller = req.body.isSeller;
+        user.image = user.gravatar();
+        user.isSeller = req.body.isSeller;
 
         User.findOne({email: req.body.email.toLowerCase()}, (err, userStored) => {
             if(userStored)
@@ -63,6 +63,65 @@ module.exports =
                     res.json({success: true, message: "User login!", token: token});
                 }
             }   
+        });
+    },
+
+    getProfile: function(req, res, next)
+    {
+        User.find({_id: req.decoded.user._id}, (err, user) => {
+            if(err)
+                throw err;
+            res.json({success: true, message: "Successful", user});
+        });
+    },
+
+    updateProfile: function(req, res, next)
+    {
+        User.findOne({_id: req.decoded.user._id}, (err, user) => {
+            if(err)
+                return next(err);
+            
+            if(req.body.name) user.name = req.body.name;
+            if(req.body.email) user.email = req.body.email;
+            if(req.body.email) user.password = req.body.password;
+
+            user.isSeller = req.body.isSeller;
+
+            user.save();
+            res.json({
+                success: true,
+                message: 'Profile edited successfully'
+            });
+        });
+    },
+
+    getAddress: function(req, res, next)
+    { 
+        User.findOne({_id : req.decoded.user._id}, (err, user) => {
+            if(err) 
+                return next(err);
+            console.log(user);
+            res.json({success: true, message: 'Successful', address: user.address});
+        })
+    },
+
+    updateAddress: function(req, res, next)
+    {
+        User.findOne({_id: req.decoded.user._id}, (err, user) => {
+            if(err) 
+                return next(err);
+            console.log(user);
+            console.log(user.address);
+            if(req.body.addr1) user.address.addr1 = req.body.addr1;
+            if(req.body.addr2) user.address.addr2 = req.body.addr2;
+            if(req.body.city) user.address.city = req.body.city;
+            if(req.body.telephone1) user.address.telephone1 = req.body.telephone1;
+            if(req.body.telephone2) user.address.telephone2 = req.body.telephone2;
+            if(req.body.state) user.address.state = req.body.state;
+            if(req.body.postalCode) user.address.postalCode = req.body.postalCode;
+
+            user.save();
+            res.json({success: true, message: 'Address edited succesfully', address: user.address})
         });
     }
 }
--- routes/account.js
+++ routes/account.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
+var checkJWT = require('../middleware/check-jwt');
 
 const User = require('../models/user');
 const UserController = require('../controllers/account');
@@ -9,4 +10,12 @@ router.get('/signup', UserController.home);
 router.post('/signup', UserController.saveUser);
 router.post('/login', UserController.loginUser);
 
+router.route('/profile')
+    .get(checkJWT, UserController.getProfile)
+    .post(checkJWT, UserController.updateProfile);
+
+router.route('/address')
+    .get(checkJWT, UserController.getAddress)
+    .post(checkJWT, UserController.updateAddress);
+    
 module.exports = router;
