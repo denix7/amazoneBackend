a4905db2d72f4d3f813713102cef7a612178f17c
Denix7 <dennis.brian.pv@gmail.com>
2020-01-28 19:25:33 -0400

    Mejoras para almacenar y obtener una imagen de product

--- controllers/product.js
+++ controllers/product.js
@@ -2,6 +2,7 @@ var Product = require('../models/product');
 var User = require('../models/user');
 var config = require('../config');
 var fs = require('fs');
+var path = require('path');
 var faker = require('faker');
 
 module.exports = 
@@ -18,38 +19,23 @@ module.exports =
         product.cantity = req.body.cantity;
         product.description = req.body.description;
         product.valoracion = req.body.valoracion;
-        product.image = null;
         product.owner = req.decoded.user._id;
         product.category = req.body.categoryId;
-
-        if(req.files)
+        
+        if(req.body.image)
         {
-            var file_path = req.files.image.path;
-            var file_split = file_path.split('\\');
-            var file_name = file_split[2];
-            var ext_split = file_name.split('\.');
-            var file_ext = ext_split[1];
-    
-            if(isValidExtension(file_ext))
-            {
-                product.image = file_name;
-                product.save();
-                res.json({
-                    success: true,
-                    message: 'Successfully Added the product'
-                });  
-            }
-            else
-            {
-                fs.unlink(file_path, (err) => {
-                    return res.status(200).send({message: "La extension no es valida"});
-                }); 
-            }
+            product.image = req.body.image;
         }
         else
         {
-            return res.status(200).send({message: 'No se han subido imagenes'});
+            product.image = null;
+
         }
+
+        product.save();
+        res.json({
+            success: true,
+            message: 'Successful added 10 products'});
     },
 
     getProducts(req, res, next)
@@ -62,7 +48,11 @@ module.exports =
             if(err)
                 return next(err);
 
-            res.json({success: true, message: 'Successful', products:productsStored});
+            res.json({
+                success: true,
+                message: 'Successful', 
+                products:productsStored
+            });
         });
     },
 
@@ -83,6 +73,86 @@ module.exports =
         }
 
         res.json({message: 'Successful added 10 products'});
+    },
+
+    uploadImage(req, res, next)
+    {
+        var file_name = 'Imagen no subida...';
+
+        if(!req.files)
+        {
+            return res.status(404).send({
+                status: 'error',
+                message: file_name
+            });
+        }
+
+            var file_path = req.files.file0.path;
+            console.warn(file_path);
+            var file_split = file_path.split('\\');
+            console.warn(file_split);
+            var file_name = file_split[2];
+            var ext_split = file_name.split('\.');
+            var file_ext = ext_split[1];
+
+            if(!isValidExtension(file_ext))
+            {
+                fs.unlink(file_path, (err) => {
+                    return res.status(200).send({message: 'La extension no es valida'})
+                });
+            }
+            else{
+                var articleId = req.params.id;
+                if(articleId)
+                {
+                    Product.findByIdAndUpdate(productId, {image: file_name}, {new:true}, (err, productUpdated) => {
+                        if(err)
+                        {
+                            return res.status(500).send({message: 'Error en la peticion'});
+                        }
+                        else if(!productUpdated)
+                        {
+                            return res.status(404).send({message: 'No se ha podido actualizar'});
+                        }
+                        else
+                        {
+                            return res.status(200).send({
+                                status: 'success',
+                                product: productUpdated})
+                        }
+                    });
+                }
+                else
+                {
+                    return res.status(200).send({
+                        status: 'success',
+                        image: file_name
+                    });
+                }
+            }
+    },
+
+    getImage(req, res)
+    {
+        var file = req.params.image;
+        var path_file = './uploads/products/' + file;
+
+        console.warn(path_file);
+        
+
+        fs.exists(path_file, (exists) => {
+            if(exists)
+            {
+                return res.sendFile(path.resolve(path_file));
+            }
+            else
+            {
+                return res.status(404).send({
+                    status : 'error',
+                    message: 'this image does not exists'
+                });
+            }
+        });
     }
 }
 
--- routes/product.js
+++ routes/product.js
@@ -11,7 +11,10 @@ const md_upload = multiparty({uploadDir: './uploads/products'});
 var checkJWT = require('../middleware/check-jwt');
 
 router.get('/', checkJWT, ProductController.getProducts);
-router.post('/', checkJWT, [md_upload], ProductController.createProduct);
+router.post('/', checkJWT, ProductController.createProduct);
+router.post('/', checkJWT, ProductController.createProduct);
+router.post('/upload-image/:id?', [md_upload], ProductController.uploadImage);
+router.get('/get-image/:image', ProductController.getImage);
 router.get('/faker/test', ProductController.faker);
 
 module.exports = router;
