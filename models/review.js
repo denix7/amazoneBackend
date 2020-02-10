4dbe830f0e34849581c367740f2ff04ecf3fc559
Denix7 <dennis.brian.pv@gmail.com>
2020-02-10 19:36:24 -0400

    Implementado el modelo Review para productos

---
+++ models/review.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const mongoose = require('mongoose');
+
+const {Schema} = mongoose;
+
+const CategorySchema = new Schema({
+    owner: {type: Schema.Types.ObjectId, ref: 'User'},
+    title: { type: String},
+    description: { type: String},
+    rating: { type: Number, default: 0},
+    createdAt: { type: Date, default: Date.now}
+});
+
+module.exports = mongoose.model('Review', ReviewSchema);
