502fb54d241d0e02fd77df8ca1bf0cdb7b8a833c
Denix7 <dennis.brian.pv@gmail.com>
2020-02-11 19:49:53 -0400

    Utilizando virtuals para calcular el average del rating, averageRating no persiste en BD

--- models/product.js
+++ models/product.js
@@ -15,6 +15,28 @@ const ProductSchema = new Schema({
     category: {type: Schema.Types.ObjectId, ref:'Category'},
     reviews: [{type: Schema.Types.ObjectId, ref:'Review'}],
     createdAt: {type: Date, default: Date.now}
+}, {
+    otObject: { virtuals: true },
+    toJSON: { virtuals: true }
 });
+
+ProductSchema
+    .virtual('averageRating')
+    .get(function(){
+        var rating = 0;
+
+        if(this.reviews.length == 0)
+            rating = 0;
+        else
+        {
+            this.reviews.map((review) => {
+                rating += review.rating;
+            });
+
+            rating = rating / this.reviews.length;
+        }
+
+        return rating;
+    });
 
 module.exports = mongoose.model('Product', ProductSchema);
