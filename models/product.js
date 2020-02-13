3e31bef621261f1321dcc6ab16a78e0aa861a104
Denix7 <dennis.brian.pv@gmail.com>
2020-02-12 22:06:34 -0400

    instalando y configurando algolia para busquedas avanzadas

--- models/product.js
+++ models/product.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const mongoose = require('mongoose');
+const mongooseAlgolia = require('mongoose-algolia');
 
 const { Schema } = mongoose;
 
@@ -39,4 +40,47 @@ ProductSchema
         return rating;
     });
 
-module.exports = mongoose.model('Product', ProductSchema);
+    ProductSchema.plugin(mongooseAlgolia, {
+        appId: 'SE9V63KLJ2',
+        apiKey: '9a7675d96631b9325654edb240869dce',
+        indexName: 'amazonev1',
+        selector: '_id title image reviews description price owner created averageRating',
+        populate: {
+            path: 'owner reviews',
+            select: 'name rating'
+        },
+        defaults: {
+            author: 'uknown'
+        },
+        mappings: {
+            title: function(value) {
+                return `${value}`
+            }
+        },
+        virtuals: {
+            averageRating: function(doc) {
+                var rating = 0;
+
+                if(doc.reviews.length == 0)
+                    rating = 0;
+                else
+                {
+                    doc.reviews.map((review) => {
+                        rating += review.rating;
+                    });
+
+                    rating = rating / doc.reviews.length;
+                }
+
+                return rating;
+            }
+        },
+        debug: true 
+    })
+
+    let Model = mongoose.model('Product', ProductSchema);
+    Model.SyncToAlgolia();
+    Model.SetAlgoliaSettings({
+        searchableAttributes: ['title']
+    });
+module.exports = Model; 
